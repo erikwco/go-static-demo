b8c0ec97681ea41426d5c149fd548dbeaa6e7d85
Erik Chacón <erikchacon@me.com>
2022-09-17 01:02:01 -0600

    templates implementation

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
+
+var epoch = time.Unix(0, 0).Format(time.RFC1123)
+
+var noCacheHeaders = map[string]string{
+	"Expires":         epoch,
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+}
+
+var etagHeaders = []string{
+	"ETag",
+	"If-Modified-Since",
+	"If-Match",
+	"If-None-Match",
+	"If-Range",
+	"If-Unmodified-Since",
+}
 
 func main() {
 	// create http file system
-	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", NoCache(http.StripPrefix("/static/", http.FileServer(http.Dir("./static")))))
 
-	// start listening and root
-	http.Handle("/", fs)
+	// handler for listening root templates
+	http.HandleFunc("/", serveTemplate)
 
 	// starting server
 	log.Println("Starting server on :3000 port ...")
@@ -19,4 +42,67 @@ func main() {
 		log.Fatalf(">>> error serving %v", err.Error())
 	}
 
+}
+
+func NoCache(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Delete any ETag headers that may have been set
+		for _, v := range etagHeaders {
+			if r.Header.Get(v) != "" {
+				r.Header.Del(v)
+			}
+		}
+
+		// Set our NoCache headers
+		for k, v := range noCacheHeaders {
+			w.Header().Set(k, v)
+		}
+
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	// Return 404 if template doesn't exists
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	// return 404 if request is directory
+	// or move to index.html
+	if info.IsDir() {
+		fp = filepath.Join(fp, "index.html")
+		info, err = os.Stat(fp)
+		if err != nil {
+			if os.IsNotExist(err) {
+				http.NotFound(w, r)
+				return
+			}
+		}
+
+		if info.IsDir() {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Template solicitado no encontrado [%v]", err.Error()), 500)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		http.Error(w, "Ejecución de template errónea", 500)
+	}
 }
--- static/pro/js/main.js
+++
@@ -1,6 +0,0 @@
-const loading = () => {
-    console.log("loading function");
-}
-
-
-document.onload = loading;
